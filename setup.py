0a5dbf8b08a2aa96114df8bd6d4637ef261fbbcc
mjclark1 <mattjclark0407@hotmail.com>
2018-05-19 20:38:32 +1000

    Preparing release 1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='meitrackparser',
-    version='1.0.dev0',
+    version='1.0',
     description='A library for parsing meitrack gprs data',
     url='https://bitbucket.org/poslive/python-lib-meitrack-parser.git',
     author='Matt Clark',
